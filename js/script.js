7a7bbb5867dced2c64196196202e0097d99ddb14
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-12 01:22:48 +0000

    Added error messages for if user enters a negative number or nothing

--- js/script.js
+++ js/script.js
@@ -14,11 +14,26 @@ function numSent() {
   //getting user input for the variable userNum
   let userNum = parseInt(document.getElementById("number").value);
 
-  //using a do... while loop to find factorial
-  do {
-    facAnswer = facAnswer * counter;
-    counter = counter + 1;
-  } while (counter <= userNum);
+  //If: if user enters nothing
+  if (isNaN(userNum)) {
+    document.getElementById("results").innerHTML = "Please enter a valid number.";
+    return;
+  }
+
+  //Else if: if user enters a negative number
+  else if (userNum < 0) {
+    document.getElementById("results").innerHTML = "Please enter a positive number.";
+    return;
+  }
+
+  //Else: let the do-while loop execute to calculate the factorial
+  else {
+    //using a do... while loop to find factorial
+    do {
+      facAnswer = facAnswer * counter;
+      counter = counter + 1;
+    } while (counter <= userNum);
+  }
 
   //display factorial back to screen
   document.getElementById("results").innerHTML = "The factorial of your inputted number (!" + userNum + ") = " + facAnswer + ".";
